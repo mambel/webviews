b43f58f5220da50028cb671d59adb9f9e1358ae9
Angel Mambel <mambelangel5@gmail.com>
2022-06-19 18:08:37 -0400

    despues de configurar tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,5 @@
 
 @section('content')
     <h1>Home</h1>
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>@yield('title')</title>
     <style>
         .active  a {
@@ -14,6 +15,9 @@
     </style>
 </head>
 <body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
     @include('partials.nav')
     @yield('content')
 </body>
