7056e0d561bb9805c89264abe519bfb7416577a5
Angel Mambel <mambelangel5@gmail.com>
2022-06-28 22:01:11 -0400

    agregando grid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,15 @@
 @section('title','Home')
 
 @section('content')
-    <h1>Home</h1>
+{{--    <h1>Homeeee</h1>--}}
+    <div class="container mx-auto">
+        <div class="grid grid-cols-4">
 
+            <div class="bg-blue-100">A</div>
+            <div class="bg-blue-200">B</div>
+            <div class="bg-blue-300">C</div>
+            <div class="bg-blue-400">D</div>
+            
+        </div>
+    </div>
 @endsection
